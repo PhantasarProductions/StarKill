95fda6743596db3d4641759c55ab15bc9eb34150
Jeroen P. Broks <facebook@tbbs.nl>
2020-04-27 11:09:58 +0200

    Pack script

---
+++ pack.ps1
@@ -0,0 +1,4 @@
+jcr6 add -fc Store -cm Store -i Data -doj Data.JCR
+# This script has only been set up to quickly call JCR6
+# Since packing is not allowed, and no real scripting is to be done
+# This should suffice!
